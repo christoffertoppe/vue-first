9fa870331b301328d63fc285dce0ea82beba2468
christoffertoppe <chris.mailbox85@gmail.com>
2020-09-29 22:56:37 +0300

    Bikes and Workers shown in two different lists using Vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" class="small-container">
+    <h1>Työntekijät</h1>
+      <employee-table/>
+    <h1>Kaupunkipyörät</h1>
+      <kartta/>
+      <bikes/>
+  </div>
+</template>
+
+<script>
+  import EmployeeTable from '@/components/Tyontekijataulu.vue'
+  import Bikes from '@/components/pyorat.vue'
+  import Kartta from '@/components/kartta.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      EmployeeTable,
+      Bikes,
+      Kartta
+    },
+  }
+</script>
+
+<style>
+  h1{
+   font-style: italic;
+    margin-left: 5px;
+  }
+</style>
---
+++ src/components/kartta.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'kartta.vue',
+  };
+</script>
+
+<style scoped>
+
+</style>
